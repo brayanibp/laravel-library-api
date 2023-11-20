5b27686cd194dc2272bb6f60d11a2c2ea43d870f
Brayan Bernal <brayanbernal0710@gmail.com>
2023-11-20 11:01:58 -0400

    Adding jwt auth

--- library-api/app/Providers/AuthServiceProvider.php
+++ library-api/app/Providers/AuthServiceProvider.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Providers;
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +26,16 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+        Auth::viaRequest('jwt', function (Request $request) {
+            try{
+                $tokenPayload = JWT::decode($request->bearerToken(), new Key(config('jwt.key'), 'HS256'));
+
+                return \App\Models\User::find($tokenPayload)->first();
+            } catch(\Exception $th){
+                Log::error($th);
+                return null;
+            }
+        });
     }
 }
--- library-api/routes/api.php
+++ library-api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
 use App\Http\Controllers\StatisticsController;
+use App\Http\Controllers\ApiAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,40 +16,42 @@ use App\Http\Controllers\StatisticsController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/login/auth', function (Request $request) {
-    return $request->user();
+Route::prefix('/login')->group(function () {
+    Route::post('/register', [ApiAuthController::class, 'register']);
+    Route::post('/auth', [ApiAuthController::class, 'login']);
+    Route::middleware('auth:api')->get('/user-details', [ApiAuthController::class, 'authenticatedUserDetails']);
 });
 
-Route::get('/login/register', function (Request $request) {
-    return 'Register';
-});
 
-Route::prefix('/books')->group(function () {
-    Route::get('/read', [BooksController::class, 'getAll']);
-    Route::get('/read/get/{id}', [BooksController::class, 'getBookInfo']);
-    Route::get('/read/get/{id}/page/{page}', [BooksController::class, 'getBookFile']);
-    Route::post('/add', [BooksController::class, 'store']);
-});
-
-Route::prefix('/statistics')->group(function () {
-    Route::redirect('/', '/api/statistics/get');
-
-    // Redirecting '/' route to '/get' to obtain the list of available statistics
-    Route::get('/get', function (Request $request) {
-        return [
-            'statistics/get/most/readed/books',
-            'statistics/get/less/readed/books',
-            'statistics/get/most/popular/authors',
-            'statistics/get/less/popular/authors',
-        ];
+// applying authentication middleware
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('/books')->group(function () {
+        Route::get('/read', [BooksController::class, 'getAll']);
+        Route::get('/read/get/{id}', [BooksController::class, 'getBookInfo']);
+        Route::get('/read/get/{id}/page/{page}', [BooksController::class, 'getBookFile']);
+        Route::post('/add', [BooksController::class, 'store']);
     });
 
-    // Statistics routes
-    Route::prefix('/get')->group(function () {
-        Route::get('most/readed/books', [StatisticsController::class, 'getMostReadedBooks']);
-        Route::get('less/readed/books', [StatisticsController::class, 'getLessReadedBooks']);
-        Route::get('most/popular/authors', [StatisticsController::class, 'getMostPopularAuthors']);
-        Route::get('less/popular/authors', [StatisticsController::class, 'getLessPopularAuthors']);
+    Route::prefix('/statistics')->group(function () {
+        Route::redirect('/', '/api/statistics/get');
+
+        // Redirecting '/' route to '/get' to obtain the list of available statistics
+        Route::get('/get', function (Request $request) {
+            return [
+                'statistics/get/most/readed/books',
+                'statistics/get/less/readed/books',
+                'statistics/get/most/popular/authors',
+                'statistics/get/less/popular/authors',
+            ];
+        });
+
+        // Statistics routes
+        Route::prefix('/get')->group(function () {
+            Route::get('most/readed/books', [StatisticsController::class, 'getMostReadedBooks']);
+            Route::get('less/readed/books', [StatisticsController::class, 'getLessReadedBooks']);
+            Route::get('most/popular/authors', [StatisticsController::class, 'getMostPopularAuthors']);
+            Route::get('less/popular/authors', [StatisticsController::class, 'getLessPopularAuthors']);
+        });
     });
 });
+
